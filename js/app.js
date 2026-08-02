428c4c63067302e2c6208eeda750f11f36b4636e
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-08-02 15:17:03 +0300

    added card shuffling and dealing

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@ const player1 = {
     hand: [],
 };
 
+const player2 = { name: 'Computer 1', hand: [] };
+const player3 = { name: 'Computer 2', hand: [] };
+
+
+let Cards = [];
+let discardPile = [];
 
 
 const startButton = document.querySelector('.start');
@@ -38,8 +44,43 @@ console.log(player1.name);
 function startGame() {
     homePage.style.display = 'none';
     console.log('Game Started');
+
+    const gamePageDiv = document.querySelector('.gamePage');
+    if (gamePageDiv) gamePageDiv.style.display = 'block';
+
+    console.log('Game Started');
+    console.log("Current Player's Name:", player1.name);
+
+    shuffleCards();
+    dealCards();
 }
 
-function gamePage() {
-    gamePage.style.display = 'not none';
+
+function shuffleCards() {
+    Cards = [...cards];
+
+    for (let i = Cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [Cards[i], Cards[j]] = [Cards[j], Cards[i]];
+
+    }
+
+    console.log('Shuffle Cards:', Cards.length);
+
+}
+
+
+function dealCards() {
+    for (let i = 0; i < 7; i++) {
+        player1.hand.push(Cards.pop());
+        player2.hand.push(Cards.pop());
+        player3.hand.push(Cards.pop());
+    }
+
+
+    discardPile.push(Cards.pop());
+
+    console.log("The papers have been distributed.");
+    console.log("Player 1's hand:", player1.hand);
+    console.log("Starting Card:", discardPile[discardPile.length - 1]);
 }
