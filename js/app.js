5d74b8fbcfac754a9ab586750203ec3f7d389381
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-08-02 11:50:13 +0300

    FirstUpdate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-const
+class Card {
+    constructor(cardID, number, type, color, image) {
+        this.cardID = cardID;
+        this.number = number;
+        this.type = type;
+        this.color = color;
+        this.image = image;
+    }
+}
+
+const player1 = new Player(1, "Player 1", 0, []);
+const player2 = new Player(2, "Player 2", 0, []);
+const player3 = new Player(3, "Player 3", 0, []);
